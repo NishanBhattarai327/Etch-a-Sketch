7d0417ddb8ce89d4e2448c73f0b88a6430fab103
NishanBhattarai327 <bhattarainishan327@gmail.com>
2021-05-07 10:49:26 +0545

    Update grid size according to container

--- grid.js
+++ grid.js
@@ -1,10 +1,15 @@
-let container = document.querySelector("#container");
-
-for (let i = 0; i < 6; i++) {
-    const grid = document.createElement("div");
-    grid.setAttribute("class", "grid");
-    grid.addEventListener("mouseover", () => {
-        grid.style.background = "red";
-    });
-    container.appendChild(grid);
+const createGrid = (size, box) => {
+    for (let i = 0; i < size * size; i++) {
+        let gridSize = (500 / size).toString() + "px";
+        const grid = document.createElement("div");
+        grid.setAttribute("class", "grid");
+        grid.style.width = gridSize;
+        grid.style.height = gridSize;
+        grid.addEventListener("mouseover", () => {
+            grid.style.background = "red";
+        });
+        box.appendChild(grid);
+    }
 }
+const container = document.querySelector("#container");
+createGrid(5, container);
